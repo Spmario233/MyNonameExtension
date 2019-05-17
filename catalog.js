1ade29f59be31f537a32a1cf277b7e82466f1e8f
Spmario233 <Spmario233@163.com>
2019-05-17 22:07:36 +0800

    Add files via upload

---
+++ catalog.js
@@ -0,0 +1,15 @@
+extension["新服杂碎"]={
+	intro:"关于但不限于新服的各种玩意儿<br>更新日期：2019.05.17",
+	author:"苏婆玛丽奥",
+	netdisk:"",
+	forum:"",
+	version:"2.3",
+	files:["lvqian.jpg","extension.js","liuyan.jpg","wangcan.jpg","re_yuji.jpg","sp_taishici.jpg",
+		"package.js","re_jsp_pangtong.jpg","lvdai.jpg","duji.jpg","zhangqiying.jpg","lvkai.jpg",
+		"majun.jpg","zhanggong.jpg","weiwenzhugezhi.jpg","xf_yiji.jpg","re_zhangliang.jpg",
+		"caoying.jpg","fanchou.jpg","guosi.jpg","lijue.jpg","xurong.jpg","simahui.jpg","shuijing_card.jpg",
+		"liuyao.jpg","zhangji.jpg","panjun.jpg","yanjun.jpg","zhoufang.jpg","wolong_card.jpg",
+		"baosanniang.jpg","fengchu_card.jpg","zhaotongzhaoguang.jpg","xuanjian_card.jpg",
+		"pangdegong.jpg"],
+	size:"6.26MB"
+};
