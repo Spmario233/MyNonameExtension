226bfbb6820bab6e69e0fd6562bbd287c104aa94
Spmario233 <Spmario233@163.com>
2019-05-23 13:13:43 +0800

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -1,9 +1,9 @@
 extension["新服杂碎"]={
-	intro:"关于但不限于新服的各种玩意儿<br>更新日期：2019.05.18",
+	intro:"关于但不限于新服的各种玩意儿<br>更新日期：2019.05.23",
 	author:"苏婆玛丽奥",
 	netdisk:"",
 	forum:"",
-	version:"2.4",
+	version:"2.5",
 	files:["lvqian.jpg","extension.js","liuyan.jpg","wangcan.jpg","re_yuji.jpg","sp_taishici.jpg",
 		"re_jsp_pangtong.jpg","lvdai.jpg","duji.jpg","zhangqiying.jpg","lvkai.jpg",
 		"majun.jpg","zhanggong.jpg","weiwenzhugezhi.jpg","xf_yiji.jpg","re_zhangliang.jpg",
