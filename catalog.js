08180a2d6a1f0d087b616a80ae3743ddb9f9a343
Spmario233 <Spmario233@163.com>
2019-06-01 21:58:07 +0800

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -1,15 +1,16 @@
 extension["新服杂碎"]={
-	intro:"关于但不限于新服的各种玩意儿<br>更新日期：2019.05.30",
+	intro:"关于但不限于新服的各种玩意儿<br>更新日期：2019.06.01",
 	author:"苏婆玛丽奥",
 	netdisk:"",
 	forum:"",
-	version:"2.7",
-	files:["xuanjian_card.jpg","extension.js","majun.jpg","lvqian.jpg","zhangji.jpg","zhanggong.jpg",
-		"panjun.jpg","liuyan.jpg","weiwenzhugezhi.jpg","yanjun.jpg","wangcan.jpg","zhoufang.jpg",
-		"wolong_card.jpg","baosanniang.jpg","fengchu_card.jpg","re_yuji.jpg","zhaotongzhaoguang.jpg",
-		"sp_taishici.jpg","liuyao.jpg","xf_yiji.jpg","pangdegong.jpg","re_jsp_pangtong.jpg",
-		"re_zhangliang.jpg","lvdai.jpg","caoying.jpg","duji.jpg","zhangqiying.jpg","fanchou.jpg",
-		"lvkai.jpg","guosi.jpg","lijue.jpg","xurong.jpg","simahui.jpg","shuijing_card.jpg",
-		"simazhao.jpg","wangyuanji.jpg"],
-	size:"6.26MB"
+	version:"2.8",
+	files:["xf_yiji.jpg","extension.js","liuyao.jpg","majun.jpg","lvqian.jpg","zhangji.jpg",
+		"zhanggong.jpg","panjun.jpg","liuyan.jpg","weiwenzhugezhi.jpg","yanjun.jpg","wangcan.jpg",
+		"zhoufang.jpg","wolong_card.jpg","baosanniang.jpg","fengchu_card.jpg","re_yuji.jpg",
+		"zhaotongzhaoguang.jpg","sp_taishici.jpg","xuanjian_card.jpg",
+		"pangdegong.jpg","re_jsp_pangtong.jpg","re_zhangliang.jpg","lvdai.jpg","caoying.jpg",
+		"duji.jpg","zhangqiying.jpg","fanchou.jpg","lvkai.jpg","guosi.jpg","lijue.jpg","xurong.jpg",
+		"simahui.jpg","shuijing_card.jpg","simazhao.jpg","wangyuanji.jpg","xf_huangquan.jpg",
+		"xf_tangzi.jpg","xf_sufei.jpg"],
+	size:"6.76MB"
 };
